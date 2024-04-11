d104633bc69e7cb65703fe0d7245efa11398d5c0
Charles <vvvv4577@gmail.com>
2024-04-11 18:28:32 +0800

    header-React done

--- np/pages/test-all.js
+++ np/pages/test-all.js
@@ -1,11 +1,13 @@
 // pages/class-card.js
 import React from "react";
+import Header from "../components/header";
 import ClassCard from "../components/class_file/class-card-web";
 import Footer from "../components/footer";
 
 const ClassCardPage = () => {
   return (
     <>
+      <Header />
       <ClassCard />
       <Footer />;
     </>
