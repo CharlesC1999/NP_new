7f2a2e0c762bd9746ee2aaa9e2bb24f78b94d910
Charles <vvvv4577@gmail.com>
2024-04-13 16:26:17 +0800

    class classification react done

--- np/pages/test-all.js
+++ np/pages/test-all.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "@/components/header";
+import ClassClassifacion from "@/components/class_file/class-classification";
 import ClassFilter from "@/components/class_file/class-filter";
 import ClassCard from "@/components/class_file/class-card-web";
 import ClassCardMobileList from "@/components/class_file/class-card-mobile-list";
@@ -17,6 +18,7 @@ const ForTest = () => {
   return (
     <div style={containerStyle}>
       <Header />
+      <ClassClassifacion />
       <ClassFilter />
       <ClassCard />
       <ClassCardMobileList />
