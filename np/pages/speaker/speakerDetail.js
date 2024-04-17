e737a1beec969c71e04b819f13baa976202d236b
Jess <candylee022365@gmail.com>
2024-04-17 23:33:33 +0800

    speaker pages 進度 50%

---
+++ np/pages/speaker/speakerDetail.js
@@ -0,0 +1,10 @@
+import SpeakerCardVertical from "@/components/speaker/speaker-list/speakerCardVertical";
+import Breadcrumbs from "@/components/Breadcrumbs";
+
+export default function Speaker() {
+  return (
+    <>
+      <Breadcrumbs />
+    </>
+  );
+}
