78d7ea810e39b9ce0d21c4a9a1f0fd5dfaf3d2f7
Dorami <jj5678jj5678jj5678@gmail.com>
2024-04-18 21:47:47 +0800

    組合看看2

--- np/pages/member/memberPageMain2.js
+++ np/pages/member/memberPageMain2.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MemberPageMain2 from "@/components/member/memberPageMain2";
+import Header from "@/components/header"
+import Footer from "@/components/footer";
+import Breadcrumbs from "@/components/Breadcrumbs";
 
 
 
 export default function Member() {
     return (
       <>
+      <Header/>
+      <Breadcrumbs/>
        <MemberPageMain2/>
+       <Footer/>
       </>
     );
   }
