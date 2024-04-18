562bfe2c6bdaceb76ae307c7715f317d9704643d
John <john@test.com>
2024-04-19 00:49:09 +0800

    把收藏的愛心移到右邊顯眼處

--- np/components/recipe/detail/detail-top.js
+++ np/components/recipe/detail/detail-top.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./detail-top.module.scss";
 
 export default function DetailTop() {
+
   return (
     <>
       <div className={`row ${styles["top"]}`}>
@@ -29,18 +30,6 @@ export default function DetailTop() {
                   className={`${styles["left-bottom"]} d-flex justify-content-center justify-content-xxl-start`}
                 >
                   <p className={styles["figma-p"]}>分類：主食 / 2023-10-08</p>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={16}
-                    height={15}
-                    viewBox="0 0 16 15"
-                    fill="none"
-                  >
-                    <path
-                      d="M14.0454 1.37628C12.3805 -0.0117086 9.9045 0.237952 8.37635 1.7805L7.77785 2.38384L7.17935 1.7805C5.65423 0.237952 3.17516 -0.0117086 1.5103 1.37628C-0.397615 2.96935 -0.497872 5.82856 1.20953 7.55538L7.0882 13.4937C7.46796 13.8771 8.08469 13.8771 8.46445 13.4937L14.3431 7.55538C16.0536 5.82856 15.9533 2.96935 14.0454 1.37628Z"
-                      fill="#C14D3C"
-                    />
-                  </svg>
                 </div>
               </div>
             </div>
@@ -50,6 +39,27 @@ export default function DetailTop() {
                 className={`${styles["info-right"]} d-flex justify-content-center justify-content-xxl-start`}
               >
                 <div className={`${styles["small-card"]} ms-xl-auto`}>
+                  <div
+                    title="點擊加入收藏"
+                    className={`${styles["heart"]} h-100`}
+                    
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={16}
+                      height={15}
+                      viewBox="0 0 16 15"
+                      fill="none"
+                      className="h-100"
+                    >
+                      <path
+                        d="M14.0454 1.37628C12.3805 -0.0117086 9.9045 0.237952 8.37635 1.7805L7.77785 2.38384L7.17935 1.7805C5.65423 0.237952 3.17516 -0.0117086 1.5103 1.37628C-0.397615 2.96935 -0.497872 5.82856 1.20953 7.55538L7.0882 13.4937C7.46796 13.8771 8.08469 13.8771 8.46445 13.4937L14.3431 7.55538C16.0536 5.82856 15.9533 2.96935 14.0454 1.37628Z"
+                        fill="#C14D3C"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <div className={styles["small-card"]}>
                   <div className={styles["person"]}>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
