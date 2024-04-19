115a5b6b1b631c7bb20e810e6d7b6f555be4f213
John <john76391@gmail.com>
2024-04-19 17:16:25 +0800

    食譜列表頁的愛心換成react-icons

--- np/components/recipe/list/RecipeCardsList.js
+++ np/components/recipe/list/RecipeCardsList.js
@@ -1,8 +1,21 @@
-import React from "react";
+import { useState } from "react";
+import { FaHeart } from "react-icons/fa";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./RecipeCardsList.module.scss";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function RecipeCardsList() {
+  const [saved, setSaved] = useState(false);
+
+  const notify = () => {
+    if (saved) {
+      setSaved(false);
+      return toast("成功移除收藏");
+    } else {
+      setSaved(true);
+      return toast("成功加入收藏");
+    }
+  };
   return (
     <>
       <div className={`${styles["list-layout"]} col`}>
@@ -27,8 +40,8 @@ export default function RecipeCardsList() {
                   >
                     羊肉燉湯
                   </div>
-                  <div className={styles["heart"]}>
-                    <svg
+                  <div>
+                    {/* <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width={19}
                       height={18}
@@ -39,7 +52,15 @@ export default function RecipeCardsList() {
                         d="M17.1554 2.07986C15.1219 0.394444 12.0976 0.697603 10.2311 2.57069L9.50008 3.30333L8.76906 2.57069C6.90624 0.697603 3.87823 0.394444 1.84472 2.07986C-0.485659 4.01431 -0.608115 7.4862 1.47735 9.58305L8.65773 16.7939C9.12158 17.2595 9.87487 17.2595 10.3387 16.7939L17.5191 9.58305C19.6083 7.4862 19.4858 4.01431 17.1554 2.07986Z"
                         fill="#D9D9D9"
                       />
-                    </svg>
+                    </svg> */}
+                    <FaHeart
+                      onClick={() => {
+                        notify();
+                      }}
+                      className={`${styles["heart"]} ${
+                        saved ? styles["saved"] : styles["unsaved"]
+                      }`}
+                    />
                   </div>
                 </div>
                 <div
@@ -106,6 +127,7 @@ export default function RecipeCardsList() {
           </div>
         </section>
       </div>
+      <Toaster position="bottom-right" reverseOrder={false} />
     </>
   );
 }
