6870e117e99494b133f3e74c3c21545c4f3da7b0
wei <lucas060sss@gmail.com>
2024-04-20 14:27:39 +0800

    a

--- np/pages/_app.js
+++ np/pages/_app.js
@@ -1,11 +1,7 @@
 import "@/styles/globals.css";
-<<<<<<< HEAD
-// import "@/node_modules/bootstrap/dist/css/bootstrap.min.css"
-=======
 //  Datepicker
 import "react-datepicker/dist/react-datepicker.css";
 import "@/styles/class_styles/CustomDateTimePicker.css";
->>>>>>> 8b22a0b74cea772653914e27dfe2e8855f17aea5
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
