6ea4c7ae0e78133ffdae48d83866076743cd96c9
Dorami <jj5678jj5678jj5678@gmail.com>
2024-04-21 19:44:07 +0800

    改sidebar手機板一些兼具

--- np/components/member/Sidebar.jsx
+++ np/components/member/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"; 
-import styles from "./sidebar.module.css"
+import styles from "./Sidebar.module.css"
 
 const Sidebar =() => {
   return(
