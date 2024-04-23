60963f304ffe6e6072fbd6a2ef15601ccf54171f
Charles <vvvv4577@gmail.com>
2024-04-23 13:33:02 +0800

    OK改寫完畢

--- np/contexts/AuthContext.js
+++ np/contexts/AuthContext.js
@@ -5,9 +5,20 @@ const AuthContext = createContext(null);
 export const AuthProvider = ({ children }) => {
   // 初始化時從localStorage獲取token來設定登入狀態
   const [auth, setAuth] = useState({
-    token: localStorage.getItem("token"),
-    isLoggedIn: !!localStorage.getItem("token"), // 使用 !! 來將token轉化為布爾值
+    token: null,
+    isLoggedIn: false,
   });
+
+  useEffect(() => {
+    // 組件掛載後，從localStorage中讀取token並更新狀態
+    const token = localStorage.getItem("token");
+    if (token) {
+      setAuth({
+        token: token,
+        isLoggedIn: true,
+      });
+    }
+  }, []);
 
   const login = (token) => {
     setAuth({ token, isLoggedIn: true });
@@ -16,23 +27,6 @@ export const AuthProvider = ({ children }) => {
     // 用localStorage存儲會有安全性問題，因為localStorage是存儲在瀏覽器中，
     // 任何人都可以訪問localStorage，所以可以用cookie來存儲token
   };
-
-  // 監聽localStorage的變化來更新狀態
-  useEffect(() => {
-    const handleStorageChange = () => {
-      const token = localStorage.getItem("token");
-      setAuth({
-        token: token,
-        isLoggedIn: !!token,
-      });
-    };
-
-    window.addEventListener("storage", handleStorageChange);
-
-    return () => {
-      window.removeEventListener("storage", handleStorageChange);
-    };
-  }, []);
 
   const logout = () => {
     setAuth({ token: null, isLoggedIn: false });
