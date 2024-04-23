44fe9edcef6a7419528a5ef8aefa91eb51043a3f
YUN <a0955894356@gmail.com>
2024-04-24 00:09:27 +0800

    1

--- np/components/speaker/speaker-detail/SpeakerProfileSection.js
+++ np/components/speaker/speaker-detail/SpeakerProfileSection.js
@@ -1,17 +1,16 @@
-import React from "react";
-import styles from "./SpeakerProfileSection.module.scss";
+import React from "react"
+import styles from "./SpeakerProfileSection.module.scss"
 
-function SpeakerProfileSection({ description, experience, license }) {
-  return (
-    <div className={styles.profileSection}>
-      <h4>關於我</h4>
-      <p>{description}</p>
-      <h4>經歷</h4>
-      <p>{experience}</p>
-      <h4>證照</h4>
-      <p>{license}</p>
-    </div>
-  );
+function SpeakerProfileSection() {
+  return(<div className={styles.profileSection}>
+  <h4>關於我</h4>
+  <p>
+    50歲卸下福華飯店主廚職務，步入高雄餐旅大學的教學殿堂 20
+    多年，其間還遠赴法國、擔任藍帶客座大師講堂教師，桃李滿天下，把中式麵點的文化傳播海內外。不久前從高雄餐旅退休，以分享喜樂的心，要把在業界和學校所累積的豐富經驗，以系列課程在廚藝教室傳承，教好做、好包、好吃的中式麵食。
+  </p>
+</div>)
 }
 
-export default SpeakerProfileSection;
+export default SpeakerProfileSection
+    
+
--- np/pages/speaker/index.js
+++ np/pages/speaker/index.js
@@ -1,28 +1,11 @@
-import React, { useState } from "react";
-import data from "@/data/speaker.json";
-import styles from "@/styles/speaker/index.module.scss";
+import React from "react";
+import styles from "./index.module.scss";
 import SpeakerCardVertical from "@/components/speaker/speaker-list/speakerCardVertical";
 import Breadcrumbs from "@/components/Breadcrumbs";
 import HeaderComponent from "@/components/Header";
-import Pagination from "@/components/pagination";
 import Footer from "@/components/Footer";
 
 export default function Speaker() {
-  const groupSpeakers = (speakers, itemsPerGroup) => {
-  return speakers.reduce((allGroups, current, index) => {
-    const groupIndex = Math.floor(index / itemsPerGroup);
-
-    // 確保當前組存在
-    if (!allGroups[groupIndex]) {
-      allGroups[groupIndex] = [];
-    }
-
-    // 將當前講師加入當前組
-    allGroups[groupIndex].push(current);
-    return allGroups;
-  }, []);
-}
-  const [speakers, setSpeaker] = useState(data);
   return (
     <>
       <HeaderComponent />
@@ -32,29 +15,12 @@ export default function Speaker() {
           <h3>講師陣容</h3>
         </div>
         <div className={styles.speakerCardContainer}>
-          {/* <div className={styles.speakerGroup}>
-            {speakers.map((speaker) => {
-              return (
-                <SpeakerCardVertical
-                  key={speaker.speaker_id}
-                  name={speaker.speaker_name}
-                  title={speaker.speaker_title}
-                />
-              );
-            })}
-          </div> */}
-          {groupSpeakers(speakers, 4).map((group, index) => (
-      <div key={index} className={styles.speakerGroup}>
-        {group.map((speaker) => (
-          <SpeakerCardVertical
-            key={speaker.speaker_id}
-            id={speaker.speaker_id}
-            name={speaker.speaker_name}
-            title={speaker.speaker_title}
-          />
-        ))}
-      </div>
-    ))}
+          <div className={styles.speakerGroup}>
+            <SpeakerCardVertical />
+            <SpeakerCardVertical />
+            <SpeakerCardVertical />
+            <SpeakerCardVertical />
+          </div>
           <div className={styles.speakerGroup}>
             <SpeakerCardVertical />
             <SpeakerCardVertical />
@@ -68,9 +34,7 @@ export default function Speaker() {
             <SpeakerCardVertical />
           </div>
         </div>
-        <Pagination/>
       </div>
-
       <Footer />
     </>
   );
