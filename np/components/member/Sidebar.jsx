257c957333947250a467bf21a3d6b64c23a18341
Dorami <jj5678jj5678jj5678@gmail.com>
2024-04-24 17:24:28 +0800

    LINK

--- np/components/member/Sidebar.jsx
+++ np/components/member/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"; 
 import styles from "./Sidebar.module.css"
+import Link from "next/link";
 
 const Sidebar =() => {
   return(
@@ -15,11 +16,11 @@ const Sidebar =() => {
     </div>
     <div className={styles.menu1}>
       <div className={styles.menu2}>
-        <div className={styles.myAccount}> <a href>我的帳戶</a></div>
-        <div className={styles.lefta}><a href>購買清單</a></div>
-        <div className={styles.lefta}><a href>優惠券</a></div>
-        <div className={styles.lefta}><a href>會員等級</a></div>
-        <div className={styles.lefta}><a href>願望清單</a></div>
+        <div className={styles.myAccount}> <Link href="/member">我的帳戶</Link></div>
+        <div className={styles.lefta}><Link href="/member/member-buy">購買清單</Link></div>
+        <div className={styles.lefta}><Link href="/member/member-coupon">優惠券</Link></div>
+        <div className={styles.lefta}><Link href="/member/member-level">會員等級</Link></div>
+        <div className={styles.lefta}><Link href="">願望清單</Link></div>
       </div>
     </div>
   </div>
