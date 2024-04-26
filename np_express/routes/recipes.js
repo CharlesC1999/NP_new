967cd36f7a9308657923c58eff523f1ebf449494
John <john76391@gmail.com>
2024-04-26 11:50:05 +0800

    食譜細節頁串上資料庫完成 (需要塞更多資料、join到相關商品問題待處理

--- np_express/routes/recipes.js
+++ np_express/routes/recipes.js
@@ -20,9 +20,9 @@ router.get('/', async function (req, res) {
 // GET - 得到單筆資料(注意，有動態參數時要寫在GET區段最後面)
 router.get('/:recipeId', async function (req, res) {
   // 轉為數字
-  const id = getIdParam(req)
+  const recipeId = req.params.recipeId
 
-  const recipe = await Recipe.findByPk(id, {
+  const recipe = await Recipe.findByPk(recipeId, {
     raw: true, // 只需要資料表中資料
   })
 
