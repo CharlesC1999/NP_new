ac8223f33c844ee423e1fee07fdfb59d1e2dc5c0
CC <vvvv4577@gmail.com>
2024-04-27 00:26:28 +0800

    目前橘子不用走的，用飛奔的，想他慢、想多看看他，再來class-page/index解註解

--- np/pages/_app.js
+++ np/pages/_app.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+//  全域樣式
 import "@/styles/globals.css";
 //  Datepicker
 import "react-datepicker/dist/react-datepicker.css";
@@ -5,22 +7,51 @@ import "@/styles/class_styles/CustomDateTimePicker.css";
 //  Contexts
 import { AuthProvider } from "../contexts/AuthContext";
 //  載入動畫context
-import { LoaderProvider } from "@/hooks/use-loader";
+import { LoaderProvider, useLoader } from "@/hooks/use-loader";
 // 自訂用載入動畫元件
 import { OrangeLoader } from "@/hooks/use-loader/components";
 // loader樣式
 import "@/styles/loader.scss";
 //fontawesome
 import "@fortawesome/fontawesome-free/css/all.css";
+// Router
+import Router from "next/router";
 
 export default function App({ Component, pageProps }) {
-  const getLayout = Component.getLayout || ((page) => page); // 提供默認佈局
+  const getLayout = Component.getLayout || ((page) => page);
 
   return (
     <AuthProvider>
-      <LoaderProvider close={4} CustomLoader={OrangeLoader}>
-        {getLayout(<Component {...pageProps} />)}
+      <LoaderProvider CustomLoader={OrangeLoader}>
+        <ManageRouteChanges>
+          {getLayout(<Component {...pageProps} />)}
+        </ManageRouteChanges>
       </LoaderProvider>
     </AuthProvider>
   );
+}
+
+function ManageRouteChanges({ children }) {
+  const { setLoading } = useLoader();
+
+  React.useEffect(() => {
+    const handleStart = () => {
+      setLoading(true);
+    };
+    const handleComplete = () => {
+      setLoading(false);
+    };
+
+    Router.events.on("routeChangeStart", handleStart);
+    Router.events.on("routeChangeComplete", handleComplete);
+    Router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      Router.events.off("routeChangeStart", handleStart);
+      Router.events.off("routeChangeComplete", handleComplete);
+      Router.events.off("routeChangeError", handleComplete);
+    };
+  }, [setLoading]);
+
+  return children;
 }
