526c756d744dc3f75a3d089047d1af82e508b141
John <john@test.com>
2024-04-27 23:54:27 +0800

    食譜列表分頁功能完成 (pagination的UI尚未完成)

--- np_express/routes/recipes.js
+++ np_express/routes/recipes.js
@@ -14,15 +14,40 @@ import db from '#configs/mysql.js'
 
 // GET - 得到所有資料
 router.get('/', async function (req, res) {
-  const recipes = await Recipe.findAll({ logging: console.log })
-  // 處理如果沒找到資料
+  // 改成用raw-sql加上join串食譜跟分類
+  // const recipes = await Recipe.findAll({ logging: console.log })
 
-  const sqlCate =
-    'SELECT r.*, rcs.Recipe_cate_Name FROM recipe AS r JOIN recipe_categories AS rcs ON r.recipe_category__i_d = rcs.recipe_cate__i_d'
+  // 從網址查詢字串解構的值
+  const { page = 1, perpage = 5 } = req.query
 
-  const [rows] = await db.query(sqlCate)
+  // 分頁用
+  // page預設為1，perpage預設為3
+  const perpageNow = Number(perpage) || 5
+  const pageNow = Number(page) || 1
+  const limit = perpageNow
+  const offset = (pageNow - 1) * perpageNow
 
-  //得到所有食譜分類名稱
+  // 食譜join分類表
+  const sqlCate = `
+  SELECT r.*, rcs.Recipe_cate_Name 
+  FROM recipe AS r JOIN recipe_categories AS rcs 
+  ON r.recipe_category__i_d = rcs.recipe_cate__i_d 
+  LIMIT ${limit} OFFSET ${offset}
+  `
+  console.log('pagination: ' + sqlCate)
+
+  const sqlCountCate = `SELECT COUNT(*) AS countCate FROM recipe`
+
+  // 食譜join分類表查詢結果
+  const [recipesRawSql] = await db.query(sqlCate)
+  // 食譜join分類表總筆數(分頁用)
+  const [countCateRawSql] = await db.query(sqlCountCate)
+  // 回傳總筆數
+  const total = countCateRawSql[0].countCate
+  // 計算分頁所需的頁數
+  const pageCount = Math.ceil(total / Number(perpage)) || 0
+
+  // 得到所有食譜分類名稱
   const recipesCategories = await Recipe_Categories.findAll({
     logging: console.log,
   })
@@ -30,7 +55,7 @@ router.get('/', async function (req, res) {
   // 標準回傳JSON
   return res.json({
     status: 'success',
-    data: { recipes, recipesCategories, products: rows },
+    data: { recipesCategories, recipesRawSql, total, pageCount },
   })
 })
 
