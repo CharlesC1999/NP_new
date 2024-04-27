c11737338866a4413d1646ab5f31612bf2c734db
John <john76391@gmail.com>
2024-04-27 17:24:24 +0800

    把食譜與食譜分類資料庫串在一起

--- np_express/routes/recipes.js
+++ np_express/routes/recipes.js
@@ -17,13 +17,21 @@ router.get('/', async function (req, res) {
   const recipes = await Recipe.findAll({ logging: console.log })
   // 處理如果沒找到資料
 
+  const sqlCate =
+    'SELECT r.*, rcs.Recipe_cate_Name FROM recipe AS r JOIN recipe_categories AS rcs ON r.recipe_category__i_d = rcs.recipe_cate__i_d'
+
+  const [rows] = await db.query(sqlCate)
+
   //得到所有食譜分類名稱
   const recipesCategories = await Recipe_Categories.findAll({
     logging: console.log,
   })
 
   // 標準回傳JSON
-  return res.json({ status: 'success', data: { recipes, recipesCategories } })
+  return res.json({
+    status: 'success',
+    data: { recipes, recipesCategories, products: rows },
+  })
 })
 
 // GET - 得到單筆資料(注意，有動態參數時要寫在GET區段最後面)
