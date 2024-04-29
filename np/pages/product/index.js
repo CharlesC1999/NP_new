0619e109a04d23b694cbbd14f2235ff0ac4be1b9
YUN <a0955894356@gmail.com>
2024-04-30 00:16:27 +0800

    0430 PRODUCTTTTTTT資料串串

--- np/pages/product/index.js
+++ np/pages/product/index.js
@@ -1,5 +1,8 @@
 //React
 import React, { useState, useEffect } from "react";
+import Link from "next/link";
+
+// 然後在你的代碼中使用 `NextLink` 來代替 `Link`。
 
 //fontAwsome
 
@@ -10,6 +13,7 @@ import ProductCardList from "@/components/product/ProductCardList";
 import ProductFilter from "@/components/product/ProductFilter";
 import Footer from "@/components/Footer";
 import HeaderComponent from "@/components/Header";
+import Pagination from "@/components/pagination";
 
 // import PaginationRounded from "@/components/pagination";
 //荃做版本sideBar
@@ -25,9 +29,8 @@ import ProductSidebarDetail from "@/components/product/sideBar/ProductSidebarDet
 //style
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "@/styles/Product/product.module.css";
-
 //Data json
-import items from "@/data/product/productItems.json";
+// import items from "@/data/product/productItems.json";
 
 export default function Product() {
   const [displayGrid, setDisplayGrid] = useState(true); //選擇控制grid
@@ -44,35 +47,49 @@ export default function Product() {
     setDisplayGrid(false);
     setActiveButton("list");
   };
-  // 商品要使用的狀態，物件陣列狀態初始化值會需要至少空陣列
-  // !!注意!! 初次render(渲染)會使用初始值
-  // !!注意!! 在應用程式執行過程中，務必要保持狀態維持同樣的資料類型
-  const [products, setProducts] = useState([]);
 
-  // 與伺服器要求獲取資料的async函式
+  const [products, setProducts] = useState([]);
+  // const getProducts = async () => {
+  //   const url = "http://localhost:3005/api/my-products";
+  //   // fetch預設是使用GET，不需要加method設定
+  //   try {
+  //     const res = await fetch(url);
+  //     // 解析json格式資料成js的資料
+  //     const data = await res.json();
+  //     console.log(data);
+
+  //     //確保資料陣列，所以檢查後再設定
+  //     if (Array.isArray(data.data.products)) {
+  //       setProducts(data.data.products);
+  //     } else {
+  //       console.log("伺服器回傳資料類型錯誤，無法設定到狀態中");
+  //     }
+
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
   const getProducts = async () => {
-    const url = "http://localhost:3005/api/my-products";
-    // fetch預設是使用GET，不需要加method設定
+    const url = "http://localhost:3005/api/products";
+
+    //try{}catch{}回傳成功與失敗結果
     try {
       const res = await fetch(url);
-      // 解析json格式資料成js的資料
       const data = await res.json();
       console.log(data);
-
-      // 為了要確保資料是陣列，所以檢查後再設定
+      // 判斷是否為陣列並設置product
       if (Array.isArray(data.data.products)) {
         setProducts(data.data.products);
       } else {
-        console.log("伺服器回傳資料類型錯誤，無法設定到狀態中");
+        console.log("回傳的資料型態必須是陣列");
       }
-      // 設定到狀態中
     } catch (e) {
       console.log(e);
     }
   };
-  // 樣式2: didMount階段只執行一次
+  //樣式2出事渲染執行一次
   useEffect(() => {
-    // 頁面初次渲染之後伺服器要求資料
+    //初次渲染時執行此函式
     getProducts();
   }, []);
 
@@ -113,16 +130,24 @@ export default function Product() {
           <div
             className={`d-flex ${styles.productCard1} justify-content-between`}
           >
-            {items.map((item) => (
-              <ProductCard02
-                key={item.id}
-                id={item.id}
-                category_id={item.category_id}
-                name={item.name}
-                description={item.description}
-                price={item.price}
-              />
+            {products.map((item) => (
+              <div key={item.id}>
+                <Link href={`/product/${item.id}`}>
+                  {" "}
+                  {/* You can style this <a> tag as needed */}
+                  <ProductCard02
+                    id={item.id}
+                    category_id={item.category_id}
+                    name={item.product_name}
+                    description={item.product_description}
+                    price={item.product_price}
+                  />
+                </Link>
+              </div>
             ))}
+          </div>
+          <div className="justify-content-center d-flex">
+            <Pagination />
           </div>
         </div>
       </div>
