1fcd530c3dd082bd482a745d4219aebdc60a21d5
Jess <candylee022365@gmail.com>
2024-04-29 20:58:52 +0800

    講師細節頁麵包屑

--- np/components/speaker/speaker-detail/SpeakerCardHorizontal.js
+++ np/components/speaker/speaker-detail/SpeakerCardHorizontal.js
@@ -2,7 +2,7 @@ import React from "react"
 import styles from "./SpeakerCardHorizontal.module.scss"
 import Link from "next/link"
 function SpeakerCardHorizontal({name,description,id,image}) {
-  return (<Link href={`/speaker/detail?sid=${id}`} className={styles.speakerCardListLink}>
+  return (<Link href={`/speaker/speaker-detail?sid=${id}`} className={styles.speakerCardListLink}>
   <div className={styles.speakerCardList}>
     <div className={styles.speakerImg}>
       <img src={`/speaker-image/${image}`} alt />
