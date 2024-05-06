bc9b4adbbd081bab690fdfc1f7c0b5af38bff023
YUN <a0955894356@gmail.com>
2024-05-06 09:26:25 +0800

    product-index,70% product,60%

--- np/components/product/CateSidebar.js
+++ np/components/product/CateSidebar.js
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "@/components/product/sideBar/CateSidebar.module.css";
 
-function CateSidebar() {
+function CateSidebar({ cateName, onClick }) {
   return (
     <>
-      <div className="d-flex gap-3 flex-column mt-3">
+      <div onClick={onClick} className="d-flex gap-3 flex-column mt-3">
         <div className={`d-flex ${styles.sideBox}`}>
           <div className={styles.sideImg}>
             <a href="">
@@ -13,9 +13,9 @@ function CateSidebar() {
           </div>
           <div className={styles.sideText}>
             <a href="">
-              <h6 className={styles.left}>乳製品</h6>
+              <h6 className={styles.left}>{cateName}</h6>
             </a>
-            <h6 className={styles.right}>3</h6>
+            <h6 className={styles.right}></h6>
           </div>
         </div>
       </div>
