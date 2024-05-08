d51a6fd6740b3c32ed9f2a1d43680218c2059a77
John <john76391@gmail.com>
2024-05-08 20:12:19 +0800

    改成從localStorage取得token (避免重整後登入狀態又消失

--- np/components/member/Sidebar.js
+++ np/components/member/Sidebar.js
@@ -1,9 +1,56 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./Sidebar.module.css";
 import Link from "next/link";
 
-const Sidebar = ({ userData = { User_image: null } }) => {
+const Sidebar = () => {
+  // 取得localStorage裡的token，用來發起req帶入headers
+  const [LStoken, setLStoken] = useState("");
+  const getTokenInLS = () => {
+    setLStoken(localStorage.getItem("token"));
+  };
+
+  const [userData, setUserData] = useState({
+    id: 0,
+    User_name: "",
+    Account: "",
+    Email: "",
+    Phone: "",
+    Address: "",
+    Gender: "",
+    date_of_birth: "",
+    User_image: null,
+  });
+
+  // 串接上後端並把token傳進headers用來解碼
+  // !!! 從localStorage取出token後，帶入headers來解碼，若要用postman測試記得Authorization也要選Bearer Token並放入加密的token
+  const getUser = async () => {
+    const url = "http://localhost:3005/api/member-profile/check";
+    const tokenforheaders = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${LStoken}`,
+      },
+    };
+    try {
+      const res = await fetch(url, tokenforheaders);
+      const data = await res.json();
+      setUserData(data.data.user);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // 初次渲染時取得LS裡的token
+  useEffect(() => {
+    getTokenInLS();
+  }, []);
+
+  // 得到token後執行getUser()去後端解碼token並根據得到的user資料查詢資料庫並將資料設定給userData
+  useEffect(() => {
+    getUser();
+  }, [LStoken]);
+
   return (
     <>
       <div className={styles.menu}>
