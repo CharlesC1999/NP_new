e44a2af851e9d157e23651d2b45c959a8d2330ce
John <john76391@gmail.com>
2024-05-10 14:29:22 +0800

    修改密碼頁面補上breadcrumbs

--- np/pages/member/password-reset.js
+++ np/pages/member/password-reset.js
@@ -1,22 +1,22 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Breadcrumbs from "@/components/Breadcrumbs";
 import PasswordResetMain from "@/components/member/PasswordResetMain";
 import Sidebar from "@/components/member/Sidebar";
-import styles from "@/styles/member-styles/Container1.module.css"
-import Header from "@/components/header"
+import styles from "@/styles/member-styles/Container1.module.css";
+import Header from "@/components/header";
 import Footer from "@/components/footer";
 
-
 export default function PasswordReset() {
-    return (
-      <>
-      <Header/>
-      <div className={` ${styles.container1} ${styles.main} ` }>
-       <Sidebar/>
-       <PasswordResetMain/>
-       </div>
-       <Footer/>
-      </>
-    );
-  }
+  return (
+    <>
+      <Header />
+      <Breadcrumbs />
+      <div className={` ${styles.container1} ${styles.main} `}>
+        <Sidebar />
+        <PasswordResetMain />
+      </div>
+      <Footer />
+    </>
+  );
+}
