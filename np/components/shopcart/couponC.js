d7d6ac3c79d8b40329bff2a2f7dd5b07e98c9cbb
CC <vvvv4577@gmail.com>
2024-05-13 06:18:00 +0800

    在此呼籲，大家理性，資料庫裡面的東西還是不要亂命名喔，請保持一個尊重友善包容的氛圍，減少紛爭，有問題請針對問題，請先想出方式，互相為對方考慮，住大家都可以早睡，反正要比晚也比不贏我

--- np/components/shopcart/couponC.js
+++ np/components/shopcart/couponC.js
@@ -1,10 +1,12 @@
 import React from "react";
+import styles from "./couponC.module.scss";
 
 export default function CouponC({ couponDetail, Index }) {
   return (
     <div key={Index}>
-      <div>
+      <div className={styles.cardSetContainer}>
         <img src={`@/${couponDetail.coupon_image}`} alt="優惠券" />
+        <div className={styles.cName}>{couponDetail.c_name}</div>
       </div>
     </div>
   );
