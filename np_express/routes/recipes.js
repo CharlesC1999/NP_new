f6a75e5ae3b77c770e31d337f2fde729081ddc73
John <john76391@gmail.com>
2024-05-14 20:45:24 +0800

    我以為我有改推薦食譜的新路徑，姵云我很抱歉 :(

--- np_express/routes/recipes.js
+++ np_express/routes/recipes.js
@@ -107,6 +107,13 @@ router.get('/', async function (req, res) {
   // 計算分頁所需的頁數
   const pageCount = Math.ceil(total / Number(perpage)) || 0
 
+  // 給TopBar用的總筆數
+  const sqlCountAllTopBar = `SELECT COUNT(*) AS countCate FROM recipe`
+  // 主食
+  const sqlAllTopBar = `SELECT COUNT(*) AS countAll FROM recipe`
+  const [allCountTopBar] = await db.query(sqlAllTopBar)
+  const finalAllCountTopBar = allCountTopBar[0].countAll
+
   // 得到所有食譜分類名稱
   const recipesCategories = await Recipe_Categories.findAll({
     logging: console.log,
@@ -119,6 +126,7 @@ router.get('/', async function (req, res) {
       recipesCategories,
       recipesRawSql,
       total,
+      finalAllCountTopBar,
       pageCount,
       finalStapleCount,
       finalSauceCount,
